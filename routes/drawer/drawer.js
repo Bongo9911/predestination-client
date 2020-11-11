19d5f1c2b686f7e97d4ba2a8a56661aa7a04c9cc
Advait Scaria <ajs244@students.calvin.edu>
2020-11-10 20:39:27 -0500

    Added icons to menu stack

--- routes/drawer/drawer.js
+++ routes/drawer/drawer.js
@@ -1,19 +1,39 @@
 import * as React from 'react';
+import { Text, View } from 'react-native';
+
 import { createDrawerNavigator } from '@react-navigation/drawer';
 
 import HomeStack from '../drawer/home/homeStack';
 import AccountStack from '../drawer/account/accountStack';
 
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+
+
 const Drawer = createDrawerNavigator();
 
 /**
- * DrawerScreen is the entirety of the application once the user has logged in. 
+ * DrawerScreen is the entirety of the application once the user has logged in.
  */
 export default function DrawerScreen({ setLoginStatus, route }) {
     return (
         <Drawer.Navigator initialRouteName="Home" drawerPosition="right" drawerType="front" screenOptions={{ headerShown: false }}>
-            <Drawer.Screen name="Home" component={HomeStack} />
-            <Drawer.Screen name="Account" component={AccountStack} />
+            <Drawer.Screen
+                options={{
+                    title: 'Home',
+                    drawerIcon: () => (
+                        <Icon name="home" size={25} color="#5CDB95" />
+                    )
+                }}
+                name="Home" component={HomeStack}
+            />
+            <Drawer.Screen
+                options={{
+                    title: 'Account',
+                    drawerIcon: () => (
+                        <Icon name="account" size={25} color="#5CDB95" />
+                    )
+                }}
+                name="Account" component={AccountStack} />
         </Drawer.Navigator>
     );
 };
